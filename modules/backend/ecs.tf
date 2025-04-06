526ac801b3a77c47b128f6039048989b0a4f84b9
Hitvanshu Kevadia <110374523+leedrago03@users.noreply.github.com>
2025-04-06 23:47:53 +0530

    Update ecs.tf

--- modules/backend/ecs.tf
+++ modules/backend/ecs.tf
@@ -1,23 +1,6 @@
-locals {
-  container_name = "backend"
 
-  container_default_env = merge(
-    {
-      DATABASE_URL : var.database_url
-    },
-    {
-      S3_FILE_URL : aws_s3_bucket.uploads.bucket_regional_domain_name,
-      S3_BUCKET   : aws_s3_bucket.uploads.id,
-      S3_REGION   : aws_s3_bucket.uploads.region,
-      S3_ENDPOINT   : "https://s3.${aws_s3_bucket.uploads.region}.amazonaws.com"
-    },
-    var.redis_url != null ? { REDIS_URL : var.redis_url, CACHE_REDIS_URL : var.redis_url, EVENTS_REDIS_URL : var.redis_url, WE_REDIS_URL : var.redis_url } : {},
-    var.store_cors != null ? { STORE_CORS : var.store_cors } : {},
-    var.admin_cors != null ? { ADMIN_CORS : var.admin_cors } : {},
-    var.run_migrations != null ? { MEDUSA_RUN_MIGRATION : tostring(var.run_migrations) } : {},
-    local.create_admin_user != null ? { MEDUSA_CREATE_ADMIN_USER : tostring(local.create_admin_user) } : {}
-  )
-  container_env = merge(local.container_default_env, var.extra_environment_variables)
+locals {
+  # ... (other local variables)
 
   container_default_secrets = merge(
     {
@@ -42,6 +25,8 @@ locals {
     } : {}
   )
   container_secrets = merge(local.container_default_secrets, var.extra_secrets)
+  # ... (other local variables)
+}
 
   container_definition = {
     name           = local.container_name
