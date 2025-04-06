98261da84021e39741f48c6c69ba7706ddcbbf0e
Hitvanshu Kevadia <110374523+leedrago03@users.noreply.github.com>
2025-04-06 23:44:06 +0530

    Update iam.tf

--- modules/backend/iam.tf
+++ modules/backend/iam.tf
@@ -25,7 +25,7 @@ data "aws_iam_policy_document" "ecs_execution_policy" {
       "ecr:BatchGetImage",
       "ecr:BatchCheckLayerAvailability",
     ]
-    resources = [var.ecr_arn != null ? var.ecr_arn : "*"] # Changed to handle null
+    resources = [var.ecr_arn != null ? var.ecr_arn : "*"]
     effect    = "Allow"
   }
 
@@ -46,13 +46,13 @@ data "aws_iam_policy_document" "ecs_execution_policy" {
 }
 
 resource "aws_iam_role" "ecs_execution" {
-  name_prefix       = "${substr(var.context.project, 0, 8)}-${substr(var.context.environment, 0, 8)}-ecs-exec-" # Modified prefix
+  name_prefix       = "${substr(var.context.project, 0, 8)}-${substr(var.context.environment, 0, 8)}-ecs-exec-"
   assume_role_policy = data.aws_iam_policy_document.ecs_execution_assume_role.json
   tags              = local.tags
 }
 
 resource "aws_iam_policy" "ecs_execution" {
-  name_prefix = "${substr(var.context.project, 0, 8)}-${substr(var.context.environment, 0, 8)}-ecs-exec-" # Modified prefix
+  name_prefix = "${substr(var.context.project, 0, 8)}-${substr(var.context.environment, 0, 8)}-ecs-exec-"
   policy      = data.aws_iam_policy_document.ecs_execution_policy.json
   tags        = local.tags
 }
@@ -61,39 +61,6 @@ resource "aws_iam_role_policy_attachment" "ecs_execution" {
   role       = aws_iam_role.ecs_execution.name
   policy_arn = aws_iam_policy.ecs_execution.arn
 }
-
-data "aws_iam_policy_document" "ecs_task_assume_role" {
-  statement {
-    actions = ["sts:AssumeRole"]
-    principals {
-      type        = "Service"
-      identifiers = ["ecs-tasks.amazonaws.com"]
-    }
-    effect = "Allow"
-  }
-}
-
-data "aws_iam_policy_document" "ecs_task_policy" {
-  statement {
-    actions   = ["ssmmessages:CreateControlChannel", "ssmmessages:CreateDataChannel", "ssmmessages:OpenControlChannel", "ssmmessages:OpenDataChannel"]
-    resources = ["*"]
-    effect    = "Allow"
-  }
-}
-
-resource "aws_iam_policy" "ecs_task" {
-  name_prefix = "${substr(var.context.project, 0, 8)}-${substr(var.context.environment, 0, 8)}-ecs-task-" # Modified prefix
-  policy      = data.aws_iam_policy_document.ecs_task_policy.json
-  tags        = local.tags
-}
-
-resource "aws_iam_role_policy_attachment" "ecs_task" {
-  role       = aws_iam_role.ecs_task.name
-  policy_arn = aws_iam_policy.ecs_task.arn
-}
-
-#  Removed s3_access policy and user resources
-
 
 data "aws_iam_policy_document" "lambda_seed_assume_role" {
   count = var.seed_create ? 1 : 0
@@ -128,14 +95,14 @@ data "aws_iam_policy_document" "lambda_seed_policy" {
 
 resource "aws_iam_role" "lambda_seed" {
   count             = var.seed_create ? 1 : 0
-  name_prefix       = "${substr(var.context.project, 0, 8)}-${substr(var.context.environment, 0, 8)}-lambda-seed-" # Modified prefix
+  name_prefix       = "${substr(var.context.project, 0, 8)}-${substr(var.context.environment, 0, 8)}-lambda-seed-"
   assume_role_policy = data.aws_iam_policy_document.lambda_seed_assume_role[0].json
   tags              = local.tags
 }
 
 resource "aws_iam_policy" "lambda_seed" {
   count       = var.seed_create ? 1 : 0
-  name_prefix = "${substr(var.context.project, 0, 8)}-${substr(var.context.environment, 0, 8)}-lambda-seed-" # Modified prefix
+  name_prefix = "${substr(var.context.project, 0, 8)}-${substr(var.context.environment, 0, 8)}-lambda-seed-"
   policy      = data.aws_iam_policy_document.lambda_seed_policy[0].json
   tags        = local.tags
 }
