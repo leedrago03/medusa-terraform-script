33006a9b9d2c325ca897fa99b3386d89a793518d
Hitvanshu Kevadia <110374523+leedrago03@users.noreply.github.com>
2025-04-06 22:30:03 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -7,7 +7,6 @@ output "ecr_backend_url" {
 
 output "backend_url" {
   description = "The URL of the backend service, either from the created module or provided externally."
-  value       = var.backend_create ? module.backend[0].lb_dns_name : var.backend_url # Corrected line
+  value       = var.backend_create ? module.backend[0].lb_dns_name : var.backend_url # or application_load_balancer_dns_name
 }
-
 
