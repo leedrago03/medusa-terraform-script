50374d45124261548487a39caead4ebc54b15ace
Hitvanshu Kevadia <110374523+leedrago03@users.noreply.github.com>
2025-04-06 21:55:24 +0530

    Update lb.tf

--- modules/backend/lb.tf
+++ modules/backend/lb.tf
@@ -2,7 +2,7 @@ resource "aws_lb" "main" {
   load_balancer_type = var.load_balancer_type
   subnets            = var.vpc.private_subnet_ids
   security_groups    = [aws_security_group.lb.id]
-  name               = "${local.prefix}-lb"
+  name               = "${substr(var.context.project, 0, 8)}-${substr(var.context.environment, 0, 8)}-backend-lb" # Corrected name
   tags               = local.tags
 }
 
@@ -11,7 +11,7 @@ resource "aws_lb_target_group" "main" {
   protocol    = var.load_balancer_type == "network" ? "TCP" : "HTTP"
   vpc_id      = var.vpc.id
   target_type = "ip"
-  name        = "${local.prefix}-tg"
+  name        = "${substr(var.context.project, 0, 8)}-${substr(var.context.environment, 0, 8)}-backend-tg" # Corrected name
   health_check {
     protocol            = "HTTP"
     port                = var.container_port
@@ -22,7 +22,6 @@ resource "aws_lb_target_group" "main" {
     healthy_threshold   = var.target_group_health_check_config.healthy_threshold
     unhealthy_threshold = var.target_group_health_check_config.unhealthy_threshold
   }
-
   tags = local.tags
 }
 
@@ -33,11 +32,10 @@ resource "aws_lb_listener" "main" {
 
   default_action {
     target_group_arn = aws_lb_target_group.main.arn
-    type             = "forward"
+    type              = "forward"
   }
 
   depends_on = [aws_lb_target_group.main]
 
   tags = local.tags
 }
-
