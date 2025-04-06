3e7a9b980d128cb7d551c49816d4ddc7d7251e03
Hitvanshu Kevadia <110374523+leedrago03@users.noreply.github.com>
2025-04-06 22:28:44 +0530

    Update outputs.tf

--- modules/backend/outputs.tf
+++ modules/backend/outputs.tf
@@ -7,3 +7,7 @@ output "admin_secret_arn" {
 output "ecs_cluster_arn" {
   value = aws_ecs_cluster.main.arn
 }
+output "application_load_balancer_dns_name" {
+  description = "The DNS name of the load balancer."
+  value       = aws_lb.main.dns_name
+}
