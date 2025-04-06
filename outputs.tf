59a2d8a3afc5070b08c5064a591c8ef93e471919
Hitvanshu Kevadia <110374523+leedrago03@users.noreply.github.com>
2025-04-06 22:59:00 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,12 +1,10 @@
 output "ecr_backend_url" {
   description = "The URL of the ECR repository for the backend service, if created."
-  value       = var.ecr_backend_create ? module.ecr_backend[0].url : null
+  value       = var.ecr_backend_create ? module.ecr_backend[0].arn : null
 }
-
-
 
 output "backend_url" {
   description = "The URL of the backend service, either from the created module or provided externally."
-  value       = var.backend_create ? module.backend[0].lb_dns_name : var.backend_url # or application_load_balancer_dns_name
+  value       = var.backend_create ? module.backend[0].application_load_balancer_dns_name : var.backend_url  # CORRECTED LINE
 }
 
