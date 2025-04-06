16fc0e8c221396de26714a6037fb519649270cf2
Hitvanshu Kevadia <110374523+leedrago03@users.noreply.github.com>
2025-04-06 21:55:49 +0530

    Update elasticache.tf

--- modules/elasticache/elasticache.tf
+++ modules/elasticache/elasticache.tf
@@ -1,5 +1,5 @@
 resource "aws_elasticache_subnet_group" "main" {
-  name        = "${local.prefix}-db-subnet-group"
+  name        = "${substr(var.context.project, 0, 8)}-${substr(var.context.environment, 0, 8)}-db-subnet-group" # Corrected name
   description = "Elasticache subnet group used by ${local.prefix}."
 
   subnet_ids = var.vpc.private_subnet_ids
@@ -8,15 +8,14 @@ resource "aws_elasticache_subnet_group" "main" {
 }
 
 resource "aws_elasticache_cluster" "main" {
-  cluster_id = "${local.prefix}-cluster"
-
-  engine             = "redis"
-  node_type          = var.node_type
-  num_cache_nodes    = var.nodes_num
-  subnet_group_name  = aws_elasticache_subnet_group.main.name
-  security_group_ids = [aws_security_group.server.id]
-  port               = var.port
-  engine_version     = var.redis_engine_version
+  cluster_id           = "${substr(var.context.project, 0, 8)}-${substr(var.context.environment, 0, 8)}-cluster" # Corrected name
+  engine               = "redis"
+  node_type            = var.node_type
+  num_cache_nodes      = var.nodes_num
+  subnet_group_name    = aws_elasticache_subnet_group.main.name
+  security_group_ids   = [aws_security_group.server.id]
+  port                 = var.port
+  engine_version       = var.redis_engine_version
 
   tags = local.tags
 }
